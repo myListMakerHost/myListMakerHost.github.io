a8367bf92042ccad5b3104545fbe53086d65fd63
myListMakerHost <77997271+myListMakerHost@users.noreply.github.com>
2021-02-01 21:40:18 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -15,11 +15,22 @@ window.onload = function(){
 function populate(){
     console.log("In populate()")
     i = 0
+
+    let modeSwtich = document.getElementById("lightModeSwitch")
+
+    modeSwtich.addEventListener('change.bootstrapSwitch', function() {
+        if (modeSwtich.checked){
+            console.log("Dark")
+        }else{
+            console.log("light")
+        }
+        console.log("DO SOMETHINGSASD")
+    })    
+    
     var bodys = document.getElementById("listBtns")
     bodys.innerHTML = ""
     let buttonName = window.localStorage.key(i)
-    //window.localStorage.clear()
-    //I think i Need to add the href in the button here...
+
     while(buttonName != null){
         console.log("name: " + buttonName)
         button = document.createElement("button");
@@ -27,21 +38,19 @@ function populate(){
         linebreak = document.createElement("br");
         lineSep = document.createElement("HR");
 
-        button.className = "btn-gradient gray"
+        button.className = "btn btn-outline-primary"
+        button.style.width = "95%"
         button.innerHTML = buttonName;
         type = window.localStorage.getItem(buttonName)
-        delButton.className = "btn-gradient white"
+        delButton.className = "btn btn-outline-danger"
         delButton.innerHTML = "X";
+        delButton.style.width="5%"
         console.log(type)
         
         //I can modify this stuff by using JSON.parse() instead of indexOf
         let newType = JSON.parse(type)
         let HTMLTitle = newType.listName;
         let myEntries = newType.entries;
-
-        let startInd = type.indexOf("HTML") + 6
-        let endInd = type.length - 1
-        let htmlStr = type.slice(startInd,endInd)
 
         button.onclick = function() { 
             loadPage(HTMLTitle,myEntries); 
@@ -64,6 +73,25 @@ function populate(){
     console.log("Populated")
 }
 
+function switchMode(switchVal){
+    if (!switchVal){
+        //this is dark mode for main page
+        let myTitle = document.getElementById("homeTitle")
+        myTitle.style.color="black"
+        document.body.style.backgroundColor="white"
+        document.getElementById("nameLabel").style.color="black";
+        document.getElementById("switchText").style.color="black";
+    }else{
+        //light mode for main page
+        let myTitle = document.getElementById("homeTitle")
+        myTitle.style.color="white"
+        document.body.style.backgroundColor="rgb(27, 26, 26)";
+        document.getElementById("nameLabel").style.color="white";
+        document.getElementById("switchText").style.color="white";
+    }
+}
+
+
 //What if I just make it like this
 function loadPage(htmlTitle, listEntries){
     console.log("in load()");
@@ -72,17 +100,38 @@ function loadPage(htmlTitle, listEntries){
         mySubWindow = window.open("","_blank");
     }
     totalEntryCount = 0;
-    mySubWindow.document.body.innerHTML = ' \
-    <div style = "text-align: center;"> <h1 id="listTitle">TempName</h1> \
-    <input id = "searchBar" type="text" placeholder="Search.."> <button id="addBtn">Add</button> \
-    <ul id = "searchSuggestions"> </ul> <br> <button id="doneBtn" class="btn-gradient green">Done</button>\
-    </div> <button id="copyBtn" class="btn-gradient cyan" style="float:right">Copy</button> <br> <hr> <br>  <table id = "userList">  <tbody> <tr> <th>Rank</th> <th> Title</th> </tr> \
-    </tbody> </table>'
+    mySubWindow.document.body.innerHTML = `
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"> 
+        <link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
+        <div style="display:flex; float: left;">
+            <p id="switchText">Change Mode</p>
+
+            <label class="switch">
+                <input type="checkbox" id="lightModeSwitch">
+                <span class="slider round"></span>
+            </label>
+        </div> 
+       <br><br>
+        <div style = "text-align: center;"> 
+            <h1 id="listTitle">TempName</h1> 
+            <input id = "searchBar" type="text" placeholder="Search.."> 
+            <ul id = "searchSuggestions"> </ul> 
+            <br> 
+            <button id="doneBtn" class="btn btn-primary">Done</button>
+        </div> 
+        <button id="copyBtn" class="btn btn-primary" style="float:right">Copy</button> 
+        <br> <hr id="divider"> <br>  
+        <table id = "userList">  
+            <tbody> 
+            <tr> <th style="text-align:center;" class="tableElement">Rank</th> <th style="text-align:center;" class="tableElement"> Title</th> </tr> 
+        </tbody>
+        </table>`
     var fileRef = mySubWindow.document.createElement("link");
     fileRef.setAttribute("rel", "stylesheet");
     fileRef.setAttribute("type", "text/css");
-    fileRef.setAttribute("href", 'https://mylistmakerhost.github.io/list.css');
+    fileRef.setAttribute("href", 'https://mylistmakerhost.github.io/main.css');
     mySubWindow.document.head.appendChild(fileRef);
+    
     // Will make sure you can close a list window and open a new window
     // without having to refresh page manually
     mySubWindow.addEventListener("beforeunload",function(e){
@@ -111,7 +160,7 @@ function loadPage(htmlTitle, listEntries){
         totalEntryCount++;
     }
     
-    const input = mySubWindow.document.querySelector("input")
+    const input = mySubWindow.document.getElementById("searchBar")
     let suggestionList = mySubWindow.document.getElementById("searchSuggestions");
     let mySaveBtn = mySubWindow.document.getElementById("doneBtn");
     let myCopyBtn = mySubWindow.document.getElementById("copyBtn");
@@ -125,34 +174,53 @@ function loadPage(htmlTitle, listEntries){
         let el = mySubWindow.document.createElement('textarea');
         let listInfo = JSON.stringify(window.localStorage.getItem(htmlTitle)) //converts all JSOn info to list
         //load should add a new list entry with this string's info
-        console.log(listInfo)
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';
         el.value = listInfo;
         mySubWindow.document.body.appendChild(el);
         el.select();
         mySubWindow.document.execCommand('copy');
         mySubWindow.document.body.removeChild(el);
     }
+    let darkNum = 0
+    let darkModeCheck = mySubWindow.document.getElementById("lightModeSwitch");
+    darkModeCheck.addEventListener("click", function(){
+        darkNum++;
+        if (darkNum % 2 != 0){
+            
+            mySubWindow.document.body.style.backgroundColor = "rgb(27, 26, 26)";
+            mySubWindow.document.getElementById("switchText").style.color="white";
+            mySubWindow.document.getElementById("listTitle").style.color="white";
+            mySubWindow.document.getElementById("divider").style.color="white";
+            var all = mySubWindow.document.getElementsByClassName('tableElement');
+            for (var i = 0; i < all.length; i++) {
+                all[i].style.color = 'white';
+            }
+        }else{
+            mySubWindow.document.body.style.backgroundColor = "white";
+            mySubWindow.document.getElementById("switchText").style.color="black";
+            mySubWindow.document.getElementById("listTitle").style.color="black";
+            mySubWindow.document.getElementById("divider").style.color="black";
+            var all = mySubWindow.document.getElementsByClassName('tableElement');
+            for (var i = 0; i < all.length; i++) {
+                all[i].style.color = 'black';
+            }
 
-    let userList = mySubWindow.document.getElementById("userList")
-    
+        }
+
+    })
     mySubWindow.focus();
-
     input.addEventListener("input",updateSearch);
     function updateSearch(e){
         let movieTitleURLS = {}
-        console.log(e.target.value);
         userInp = e.target.value
-        if (userInp.length >= 3){
-            
-            
-            
+        if (userInp.length >= 3){          
             let url = "".concat(baseURL, 'configuration?api_key=', APIKEY); 
             fetch(url)
             .then((result)=>{
                 return result.json();
             })
             .then((data)=>{
-                
                 console.log('config:', data);
                 console.log('config fetched');
                 runSearch(userInp)
@@ -171,16 +239,8 @@ function loadPage(htmlTitle, listEntries){
             .then((data)=>{
                 //process the returned data
                 let result = JSON.stringify(data,null,4);
-                
-                let newResult = result;
                 let testResult = JSON.parse(result)
-                console.log("data: " + result)
-                console.log("test val: " + testResult.results[0].poster_path);
-                clearList(suggestionList); //removes previous search results
-                //should proll count amount of titles to make sure there's atleast 3
-                
-                
-                
+                clearList(suggestionList); //removes previous search results              
                 for (i = 0; i < 3; i++){
                     let movieTitle = testResult.results[i].original_title;
                     let posterURL = testResult.results[i].poster_path;
@@ -195,7 +255,6 @@ function loadPage(htmlTitle, listEntries){
                     listItem.onclick = function() {
                         input.value= "" // resets search bar
                         clearList(suggestionList) // wipes search suggestions
-                        console.log(a.textContent)
                         
                         let d = mySubWindow.document.createElement("div");
                         d.id = "container";
@@ -212,24 +271,15 @@ function loadPage(htmlTitle, listEntries){
                                 createEntry(htmlTitle, movieTitle, curPosterURL, movieId, bgColor);
                             }
                             totalEntryCount++;
-                            console.log("entry count: " + totalEntryCount)
                             let newInfo = JSON.parse(window.localStorage.getItem(htmlTitle));
-                            console.log("diff PARSED: " + newInfo);
                             newInfo.entries.push(a.textContent + "-:" + movieId + "-:" + curPosterURL)
                             window.localStorage.setItem(htmlTitle,JSON.stringify(newInfo))
-                            //call a function that creates new OBJ for dict
-                            //window.localStorage.setItem(title.textContent,createObj(newInfo,a.textContent));
                         }
-                        
                     }
-
                     suggestionList.appendChild(listItem);
                 }
-
             })
         }
-
-        
     }
 }
 // created this function for readability sake
@@ -238,7 +288,6 @@ function clearList(list){
 }
 
 //removes entry from localStorage and calls loadPage() to modify display
-// needs to update order of other elements in local storage
 function removeEntry(listName, title,titleId){
     console.log("title: " + title + " id: " + titleId)
     let listInfo = JSON.parse(window.localStorage.getItem(listName))
@@ -264,21 +313,17 @@ function removeEntry(listName, title,titleId){
 function createEntry(listTitle, entryName, urlStr, entryID, backGroundColor){
     let entry = mySubWindow.document.createElement("div");
     let userList = mySubWindow.document.getElementById("userList");
-    
     entry.id = "container";
-
     let entryImage = mySubWindow.document.createElement("img");
     entryImage.src = basePosterURL + "w92" + urlStr;
     entryImage.className = "center-img";
     entry.appendChild(entryImage);
     
-    console.log("entry image: " + entryImage.src)
-
     let dText = mySubWindow.document.createElement("div");
     let title = mySubWindow.document.createElement("h2");
     entry.style.backgroundColor=backGroundColor
     let removeBtn = mySubWindow.document.createElement("button");
-    removeBtn.className = "btn-gradient red"
+    removeBtn.className = "btn btn-outline-danger"
     removeBtn.innerHTML = "remove"
     removeBtn.onclick = function() {
         removeEntry(listTitle, entryName, entryID)
@@ -289,26 +334,29 @@ function createEntry(listTitle, entryName, urlStr, entryID, backGroundColor){
     let downArrCon = mySubWindow.document.createElement("p")
     let upArr = mySubWindow.document.createElement("i")
     let downArr = mySubWindow.document.createElement("i")
+    let upBtn = mySubWindow.document.createElement("button");
+    let downBtn = mySubWindow.document.createElement("button");
+
+    
     upArrCon.id = "arrOffset";
     downArrCon.id = "arrOffset";
-
-
+    upBtn.id = "upArrow"
+    downBtn.id ="downArrow"
 
     upArr.className = "arrow up";
     
-    upArrCon.onclick = function() {
-        console.log("do something")
+    upBtn.onclick = function() {
         changeOrder(listTitle, entryName, entryID,"up")
     }
-    downArrCon.onclick = function() {
-        console.log("down something")
+    downBtn.onclick = function() {
         changeOrder(listTitle, entryName, entryID,"down")
     }
 
     downArr.className = "arrow down";
-
-    upArrCon.appendChild(upArr);
-    downArrCon.appendChild(downArr);
+    upBtn.appendChild(upArr);
+    downBtn.appendChild(downArr);
+    upArrCon.appendChild(upBtn);
+    downArrCon.appendChild(downBtn);
 
     dText.className = "center-txt";
     title.textContent = entryName;
@@ -324,24 +372,20 @@ function createEntry(listTitle, entryName, urlStr, entryID, backGroundColor){
     let orderText = mySubWindow.document.createElement("h1");
     orderText.style.textAlign = "center";
     orderText.innerHTML = totalEntryCount + 1;
+    orderText.className="tableElement"
     tableOrder.appendChild(orderText);
     let tableTitle = mySubWindow.document.createElement("td");
     tableTitle.appendChild(entry)
     tableTitle.style.width="100%"
     tableRow.appendChild(tableOrder);
     tableRow.appendChild(tableTitle);
-
     userList.appendChild(tableRow);
-
-
 }
 
 
 function changeOrder(listName, entry_name, entry_id,direction){
     let listInfo = JSON.parse(window.localStorage.getItem(listName))
-    console.log("in change")
     let insertInd = -1
-    //COPIED SOME OF THE CODE from remove entry
     let entryList = listInfo.entries;
     // loop through all entries until found remove element from array and call loadPage()
     for (let i = 0; i < entryList.length; i++){
@@ -358,7 +402,6 @@ function changeOrder(listName, entry_name, entry_id,direction){
             }else if (direction == "down" && i != entryList.length){
                 insertInd = i + 1;
             }
-            
             if (insertInd != -1){
                 entryList.splice(insertInd,0,entryTitle +"-:" + entryID + "-:" + moviePost)
                 listInfo.entries = entryList;
@@ -367,7 +410,6 @@ function changeOrder(listName, entry_name, entry_id,direction){
                 window.localStorage.setItem(listName,JSON.stringify(listInfo));
                 loadPage(listName,listInfo.entries);
             }
-        
         break;
         }
         
@@ -377,15 +419,11 @@ function changeOrder(listName, entry_name, entry_id,direction){
 
 
 function addList(){
-    //maybe add a listener for the add button here to close the window... and calls the other function
-    //popup_window = window.open('file:///D:/Vscode%20projects/ListMaker/addForm.html','windowNew','width=300, height=300');
-    console.log("made2")
     if (document.getElementById("addForm").hidden == false){
         document.getElementById("addForm").hidden= true;
     }else{
         document.getElementById("addForm").hidden= false;
     }
-    
 }
 
 
@@ -393,12 +431,8 @@ function loadList(){
     let userLoadedList = prompt("Please paste list information");
     // I have to do it twice because it think's the output of the first parse is still a string
     let parsedInfo = JSON.parse(JSON.parse(userLoadedList))
-    console.log("parsed: " + parsedInfo.listName)
-    console.log("unparsed: " + userLoadedList)
 
     createList(parsedInfo.listName,parsedInfo.type,parsedInfo.entries)    
-
-
 }
 
 function addButton(){
@@ -407,7 +441,6 @@ function addButton(){
     
     //let genre = document.getElementById("Genre").value;
     let genre = "movie"
-    console.log("In addButton name: " + listName + " genre: " + genre)
 
     createList(listName,genre);
 }
@@ -422,22 +455,11 @@ function getListCount(){
     return (i)
 }
 
-
-
-
-
 // Adds new list to local storage and calls populate with new info
-//should change this to createEmptyList
 function createList(name, type, listEntries = []){
-    console.log("In createList set windowStorage")
 
-    //Default HTML string needs to be inserted here
-    //let temp = []
     val = {listName: name, type: type, entries: listEntries}
     
     window.localStorage.setItem(name,JSON.stringify(val)) 
-    console.log("calling populate")
-    
     populate()
-
 }
